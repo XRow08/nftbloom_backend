44c8d02e733de632389aa2c079268f5a4081178c
XRow <murilloaugustoe081@gmail.com>
2023-03-29 18:50:48 -0300

    start

--- usecase/login.js
+++ usecase/login.js
@@ -4,15 +4,12 @@ const Jwt = require("../src/services/jwt");
 const Login = (data) =>
   new Promise(async (resolve, reject) => {
     try {
-      console.log(data);
       const user = await Auth.login(data);
-      console.log(user);
       const token = await Jwt.create(user.id);
       const body = {
         token,
         user,
       };
-
       resolve(body);
     } catch (err) {
       reject(err);
